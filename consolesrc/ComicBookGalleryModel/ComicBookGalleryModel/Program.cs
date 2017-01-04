f09047ac89db211f4b35b397572ec6a9f74a29cf
Blak3Nick <blaken2021@gmail.com>
2017-01-04 08:54:55 -0500

    created first db entry

--- consolesrc/ComicBookGalleryModel/ComicBookGalleryModel/Program.cs
+++ consolesrc/ComicBookGalleryModel/ComicBookGalleryModel/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ComicBookGalleryModel.Models;
 
 namespace ComicBookGalleryModel
 {
@@ -10,6 +11,26 @@ namespace ComicBookGalleryModel
     {
         static void Main(string[] args)
         {
+            using (var context = new Context())
+            {
+                context.ComicBooks.Add(new Models.ComicBook()
+                {
+                    SeriesTitle = "The Amazing Spider-Man",
+                    IssueNUmber = 1,
+                    PublishedOn = DateTime.Today               
+                });
+                context.SaveChanges();
+
+                var comicBooks = context.ComicBooks.ToList();
+                foreach (var comicBook in comicBooks)
+                {
+                    Console.WriteLine(comicBook.SeriesTitle);
+                }
+
+                Console.ReadLine();
+            }
+
+            
         }
     }
 }
